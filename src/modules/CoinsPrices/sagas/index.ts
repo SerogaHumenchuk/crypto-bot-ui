b5a28f3152461e669dbe621212490102b6293fb7
Work <work@Serhiis-MacBook-Pro.local>
2023-08-28 14:08:10 +0300

    Removed unused sagas. Added /current-prices api call

--- src/modules/CoinsPrices/sagas/index.ts
+++ src/modules/CoinsPrices/sagas/index.ts
@@ -3,10 +3,10 @@ import { ISagaNoAction } from "../../../shared/interfaces";
 import axios from "axios";
 
 export const coinsPricesRootSaga: ISagaNoAction = function* () {
-  yield call(axios.get, "http://localhost:3001/prices", {
+  yield call(axios.get, "http://localhost:3001/diffs", {
     headers: { ["Content-Type"]: "application/json" },
   });
-  yield call(axios.get, "http://localhost:3001/diffs", {
+  yield call(axios.get, "http://localhost:3001/prices", {
     headers: { ["Content-Type"]: "application/json" },
   });
 };
